d7ef48405a459151c892e7066174ce2234907680
Luisa Tenorio <luisa.tenorio@gmail.com>
2019-02-21 19:07:02 +0100

    ajustes 2 ejercicio 1 JavasScript

--- name.js
+++ name.js
@@ -1,36 +1,53 @@
 //FASE 1
 
-var nombre = ["L","u","i","s","a","a","A"];
-
-for ( var i = 0; i< nombre.length; i++){
+var nombre = ["L","u","i","s","a"];
+ for ( var i = 0; i< nombre.length; i++){
   console.log(nombre[i]);
 }
 
-//FASE 2 + Extra
+//FASE 2
 
-var vocal = ['a','e','i','o','u' ];
+var vocal = ['a','e','i','o','u']
 
-nombre.forEach(function contains(letra){
-  if((vocal.indexOf(letra.toLowerCase()) > -1) && (isNaN(letra) === true)) 
-    {
-      console.log(letra + ": " +'vocal');
-    }else if (isNaN(letra) ===true){
-      console.log(letra + ": " +'consonant');
-    }else {
+function contains(letra) {
+		if(vocal.indexOf(letra.toLowerCase()) > -1)
+		{
+		console.log(letra +  " : " +'vocal');
+	}else{
+		console.log(letra +  " : " + 'consonante');
+		}
+}
+
+//EXTRA
+
+nombre.forEach(function(letra){ 
+    if(isNaN(letra))
+    {contains(letra);
+    }else{
       console.log(letra + ": " +'Els noms de persones no contenen números!.');
     }
-    
 });
 
-//FASE 3
 
-function frecuencias(num){
-  this[num.toLowerCase()] = (++this[num.toLowerCase()] || 1);
-  return this;
+// //FASE 3
+
+var map = new Map();
+
+for (var i = 0; i < nombre.length; i++) {
+
+var word = nombre[i];
+console.log(word);
+
+
+if(map.has(word)){
+	counter = map.get(word)+1;
+} else{
+	counter=1;
 }
-   
-console.log(nombre.map(frecuencias, {}).pop());
+map.set(word,counter);
 
+  console.log(map);
+}
 
 //FASE 4
 
@@ -41,6 +58,7 @@ var myFullName = [];
 myFullName.push(...nombre,'',...apellido);
 
 console.log(myFullName);
+
 
 
 
